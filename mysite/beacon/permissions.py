4f40f0e04d878a0a8924e63a6a8c4d83aba92fe3
meow1 <kmeo0001@student.monash.edu>
2025-09-08 17:53:40 +0800

    fix(naming): one teacher left, changed it to instructor

--- mysite/beacon/permissions.py
+++ mysite/beacon/permissions.py
@@ -4,13 +4,13 @@ from django.core.exceptions import PermissionDenied
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-from .models import User
+from beacon.models import User
 
 def instructor_required(view_func):
     @wraps(view_func)
     @login_required
     def _wrapped(request, *args, **kwargs):
-        if request.user.role == User.Role.TEACHER:
+        if request.user.role == User.Role.INSTRUCTOR:
             return view_func(request, *args, **kwargs)
         raise PermissionDenied
     return _wrapped
