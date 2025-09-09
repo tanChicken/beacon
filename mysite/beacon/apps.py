879ce178b8ab610419e920128fdbd9acf5b856df
meow1 <kmeo0001@student.monash.edu>
2025-09-09 19:04:19 +0800

    feat(signals): modify the code and add permissions for students groups as well

--- mysite/beacon/apps.py
+++ mysite/beacon/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
 
-def ready(self):
-    import beacon.signals
-
 class BeaconConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'beacon'
+
+    def ready(self):
+        import beacon.signals
