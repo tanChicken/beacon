edcf7d4338752c65dee6bfe3f01d7be3274a5c56
jtan0417 <jujutan6969@gmail.com>
2025-09-16 21:47:27 +0800

    added requirements in requirements.txt and allow host for hosting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-t5e(sw8he2v29zrp38f0!i%2n0%14=3z*ohsj+vmfxj9+pp2d9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["beacon-ejd1.onrender.com", ".onrender.com", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://beacon-ejd1.onrender.com", "https://*.onrender.com"]
 
 
 # Application definition
