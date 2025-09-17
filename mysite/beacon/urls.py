ce2024a3edee6a780dd4c71e0dc546d89112ad51
waiyan2005 <cheongwaiyan2005@gmail.com>
2025-09-17 19:25:56 +0800

    fix(course): editcss for displaying courses

--- mysite/beacon/urls.py
+++ mysite/beacon/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
     path("enrollment/", views.enrolment_page, name="enrolment_page"),
     path("enroll/<int:course_id>/", views.enrol_course, name="enrol_course"),
     path('course/<int:pk>/', views.course_detail, name='course_detail'),
-    path("lesson/<int:lesson_id>/", views.lesson_detail, name="lesson_detail"),
 ]
