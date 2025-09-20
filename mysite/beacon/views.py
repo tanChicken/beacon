5931edf7aab12618d8c828978d421a25e7417060
waiyan2005 <cheongwaiyan2005@gmail.com>
2025-09-21 00:42:35 +0800

    fix: match latest updates

--- mysite/beacon/views.py
+++ mysite/beacon/views.py
@@ -56,17 +56,14 @@ def student_signup(request):
 
         with transaction.atomic():
             user = UserModel.objects.create_user(
-                username=email,  
                 email=email,
-                first_name=first_name,
-                last_name=last_name,
                 password=password,
+                role="STUDENT",
             )
-            user.role = "STUDENT"
-            user.save()
-
-            profile, created = StudentProfile.objects.get_or_create(user=user)
+            profile = user.studentprofile
             profile.title = title
+            profile.first_name = first_name
+            profile.last_name = last_name
             profile.save()
 
         messages.success(request, "Signup successful! Please log in.")
